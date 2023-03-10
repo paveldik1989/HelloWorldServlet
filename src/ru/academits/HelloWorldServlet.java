a917cef1fd711d1b753b77f947d721ff0af9bd6c
paveldik1989 <paveldik1989@gmail.com>
2023-03-11 01:39:58 +0700

    HelloWorldServlet with 404 error

---
+++ src/ru/academits/HelloWorldServlet.java
@@ -0,0 +1,15 @@
+package ru.academits;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+public class HelloWorldServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.getOutputStream().write("Hello from HelloWorldServlet".getBytes(StandardCharsets.UTF_8));
+        response.getOutputStream().flush();
+        response.getOutputStream().close();
+    }
+}
